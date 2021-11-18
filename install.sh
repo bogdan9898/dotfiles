50fdd601dfd9151d647da0af864d0590c791579d
Istoc Bogdan <47924400+bogdan9898@users.noreply.github.com>
2021-11-18 23:32:32 +0200

    added konsave config and refactored every config file into a separte folder for each program

--- install.sh
+++ install.sh
@@ -10,9 +10,10 @@ read -p "${RED}+---------------------------------WARNING------------------------
 " continue
 
 if [ "$continue" == "y" ]; then
-	ln -svf "$(pwd)/.bashrc" ~/.bashrc
-	ln -svf "$(pwd)/.xonshrc" ~/.xonshrc
-	ln -svf "$(pwd)/.zshrc" ~/.zshrc
-	ln -svf "$(pwd)/.gitconfig" ~/.gitconfig
-	mkdir -p ~/.ssh && ln -svf "$(pwd)/.ssh/config" ~/.ssh/config
+	ln -svf "$(pwd)/bashrc/.bashrc" ~/.bashrc
+	ln -svf "$(pwd)/xonshrc/.xonshrc" ~/.xonshrc
+	ln -svf "$(pwd)/zshrc/.zshrc" ~/.zshrc
+	ln -svf "$(pwd)/gitconfig/.gitconfig" ~/.gitconfig
+	mkdir -p ~/.ssh && ln -svf "$(pwd)/ssh/config" ~/.ssh/config
+	mkdir -p ~/.config/konsave && ln -svf "$(pwd)/konsave/conf.yaml" ~/.config/konsave/conf.yaml
 fi
